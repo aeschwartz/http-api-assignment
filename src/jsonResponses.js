7f81a91c27adf5ba19c8f2a1e4471069aac11d09
Andrew Schwartz <andrewschwartz99@gmail.com>
2019-09-20 04:20:03 -0400

    Completed code so it is more in line with what the document says

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,34 +1,35 @@
-const messages = {
-  200: 'Success',
-  400: 'Bad Request',
-  401: 'Unauthorized',
-  403: 'Forbidden',
-  404: 'Not Found',
-  500: 'Internal Server Error',
-  501: 'Not Implemented',
+const ids = {
+  400: 'badRequest',
+  401: 'unauthorized',
+  403: 'forbidden',
+  404: 'notFound',
+  500: 'internalError',
+  501: 'notImplemented',
 };
 
-const respond = (res, status, type) => {
+const respond = (res, status, message, type) => {
   // eslint doesn't love me :(
   // if (!type.includes('xml') && !type.includes('json')) type = 'application/json';
 
-  res.writeHead(status, { 'Content-Type': type });
+  // type needs to be moderated, so it is set inside the switch instead of here
+  // res.writeHead(status, { 'Content-Type': type });
 
   switch (type) {
     case 'application/xml':
     case 'text/xml':
       res.writeHead(status, { 'Content-Type': type });
-      res.write(`<response><message>${messages[status]}</message><id>${status}</id></response>`);
+      res.write(`<response><message>${message}</message>${status === 200 ? "" : `<id>${ids[status]}</id></response>`}`);
       break;
     case 'application/json':
     default:
       res.writeHead(status, { 'Content-Type': 'application/json' });
-      res.write(JSON.stringify(
+      const json = status === 200 ?
+        {message: message} :
         {
-          message: messages[status],
-          id: status,
-        },
-      ));
+          message: message,
+          id: ids[status]
+        };
+      res.write(JSON.stringify(json));
       break;
   }
 
@@ -36,35 +37,34 @@ const respond = (res, status, type) => {
 };
 
 const success = (req, res, args) => {
-  respond(res, 200, args.type);
+  respond(res, 200, "This is a successful response", args.type);
 };
 
 const badRequest = (req, res, args) => {
-  if (args.valid) respond(res, 200, args.type);
-  else respond(res, 400, args.type);
+  if (args.valid === 'true') respond(res, 200, "This request has the required parameters", args.type); // have to do === 'true' bc value is string, not boolean
+  else respond(res, 400, "Missing valid query parameter set to true", args.type);
 };
 
 const unauthorized = (req, res, args) => {
-  if (args.loggedIn === 'yes') respond(res, 200, args.type);
-  else respond(res, 401, args.type);
+  if (args.loggedIn === 'yes') respond(res, 200, "You have successfully viewed the content", args.type);
+  else respond(res, 401, "Missing loggedIn query parameter set to yes", args.type);
 };
 
 const forbidden = (req, res, args) => {
-  respond(res, 403, args.type);
+  respond(res, 403, "You do not have access to this content", args.type);
 };
 
 const internal = (req, res, args) => {
-  respond(res, 500, args.type);
+  respond(res, 500, "Internal Server Error. Something went wrong.", args.type);
 };
 
 const notImplemented = (req, res, args) => {
-  respond(res, 501, args.type);
+  respond(res, 501, "A GET request for this page has not been implemented yet. Check again later for updated content.", args.type);
 };
 
 const notFound = (req, res, args) => {
-  respond(res, 404, args.type);
+  respond(res, 404, "The page you are looking for was not found.", args.type);
 };
-
 module.exports = {
   respond,
   success,
